c44dedfe47324f21e5d3a28d80e59b4736e83e4f
bobIo0l1 <45440325+bobIo0l1@users.noreply.github.com>
2019-05-30 15:19:46 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+$(function(){
+  $(window).resize(function(){
+    if(!$("#menu-button").length && $(window).width() <= 800){
+       $("#titlename").prepend(`<button id="menu-button">☰</button>`);
+    } else if($("#menu-button").length){
+      $("#menu-button").remove();
+    }
+  });
+});

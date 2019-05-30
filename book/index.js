19705e641ed139acd9da6150325369bf19d1c2b0
bobIo0l1 <45440325+bobIo0l1@users.noreply.github.com>
2019-05-30 17:06:54 -0600

    Update index.js

--- book/index.js
+++ book/index.js
@@ -8,16 +8,17 @@ $(function(){
       numHours = 0;
     }
     $("#table-times").empty();
+    $(".escape-room-time").off();
     for(i = 0; i < numHours; i++){
       let hourDecimal = parseFloat(((i*hourData-1)+startTime)%12+1);
       hourDecimal = hourDecimal.toFixed(Math.max(2, (hourDecimal.toString().split('.')[1] || []).length));
       let minutes = ("0" + ((hourDecimal % 1)*60)).slice(-2);
       console.log("hourdecimal: "+hourDecimal+" minutes "+minutes)
       $("#table-times").append(`<div class="escape-room-time">`+hourDecimal.toString().slice(0,hourDecimal.toString().indexOf(".")).concat(":",minutes)+`</div>`);
+      $(".escape-room-time").click(function(){
+        $(".escape-room-time.active").removeClass("active");
+        $(this).addClass("active");
+      });
     }
-  });
-  $(".escape-room-time").click(function(){
-    $(".escape-room-time.active").removeClass("active");
-    $(this).addClass("active");
   });
 });

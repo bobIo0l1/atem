6f0651176c9ae0ca8dd72a43f2126d1ba7cd1f22
AppleIsTheBest <45441452+AppleIsTheBest@users.noreply.github.com>
2019-06-29 13:39:12 -0400

    Create action.php

---
+++ book/action.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+</head>
+<body>
+Please confirm that this information is correct.
+<br>
+<br>
+<br>
+Your name is <?php echo $_GET["First Name"]; ?>. You would like to book <?php echo $_GET["escaperoomselect"]; ?> at <?php echo $_GET["time"]; ?>.
+</body>
